3cffc564b299c86a7d792bd89df208e7893b9504
unknown <itankit.6546@gmail.com>
2024-02-09 08:54:08 +0530

    user pages

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import register
+from .views import register , login , changePassword , address , bankDetail
 
 
 urlpatterns = [
-    path("", register, name="register")
+    path("", register, name="register"),
+    path("login/",login, name="login"),
+    path("changepassword/",changePassword, name="changepassword"),
+    path("address/",address, name="address"),
+    path("bank/",bankDetail, name="bank"),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 
 def register(request):
     return render(request, 'app/registration/register.html')
+
+
+def login(request):
+    return render(request, 'app/registration/login.html')
+
+def changePassword(request):
+    return render(request, 'app/registration/changePassword.html')
+
+
+def address(request):
+    return render(request, 'app/registration/address.html')
+
+def bankDetail(request):
+    return render(request, 'app/registration/bank.html')
+
