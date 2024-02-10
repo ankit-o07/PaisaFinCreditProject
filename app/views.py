a64c8a4e31a5666ab320c50848598defcaf6cece
unknown <itankit.6546@gmail.com>
2024-02-10 21:36:18 +0530

    Conf fix

--- app/views.py
+++ app/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
 
 
-def register(request):
-    return render(request, 'app/registration/register.html')
+# def register(request):
+#     return render(request, 'app/registration/register.html')
 
 
-def login(request):
-    return render(request, 'app/registration/login.html')
+# def login(request):
+#     return render(request, 'app/registration/login.html')
 
-def changePassword(request):
-    return render(request, 'app/registration/changePassword.html')
+# def changePassword(request):
+#     return render(request, 'app/registration/changePassword.html')
 
 
-def address(request):
-    return render(request, 'app/registration/address.html')
+# def address(request):
+#     return render(request, 'app/registration/address.html')
 
-def bankDetail(request):
-    return render(request, 'app/registration/bank.html')
+# def bankDetail(request):
+#     return render(request, 'app/registration/bank.html')
 
