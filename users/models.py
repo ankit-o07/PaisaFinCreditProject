5266499af4ade3a693a0160ca53bfc403e8e8d92
Nitin <nitin8285824551@gmail.com>
2024-02-10 16:34:44 +0530

    add verified field in user model

--- users/models.py
+++ users/models.py
@@ -14,6 +14,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    is_phone_verified = models.BooleanField(default=False)
 
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now_add=True)
