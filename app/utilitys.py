1fc8283aa42bcb6f9e4790d73db9db5cbba91da2
Nitin <nitin8285824551@gmail.com>
2024-03-13 14:27:15 +0530

    update dev env variable access

--- app/utilitys.py
+++ app/utilitys.py
@@ -3,6 +3,10 @@ from django.conf import settings
 
 
 if settings.DEBUG:
+    from dotenv import load_dotenv
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
     OTP_AUTHORIZATION_KEY = os.getenv('OTP_AUTHORIZATION_KEY')
 else:
     with open('/etc/config.json') as f:
@@ -12,6 +16,7 @@ else:
 
 def send_otp_to_phone(phone_number, otp_code, message=None):
     url = "https://www.fast2sms.com/dev/bulkV2"
+    print(OTP_AUTHORIZATION_KEY)
 
     payload = f"variables_values={otp_code}&route=otp&numbers={phone_number}"
     headers = {
